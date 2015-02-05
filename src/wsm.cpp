e64eb5503314c6fab319e980f93f33813fd199ac
Radosław Piliszek <radzio.cool@gmail.com>
2015-02-05 16:13:50 +0100

    Structure-Definition: Add code handling ancestors

--- src/wsm.cpp
+++ src/wsm.cpp
@@ -1,9 +1,23 @@
 #include "wsm.h"
 
+#include <algorithm>
+
 ostream& operator<<(ostream& os, const StructureType& o) {
-    return os << *(o.symbol);
+    os << *(o.symbol);
+
+    return os;
 }
 
 ostream& operator<<(ostream& os, const StructureDefinition& o) {
-    return os << "TODO" << " -> " << *(o.symbol);
+    if (o.ancestors) {
+        os << "(";
+        std::for_each(o.ancestors->begin(), o.ancestors->end(), [&os](const unique_ptr<StructureType>& st) {
+            os << *st;
+        });
+        os << ") -> ";
+    }
+
+    os << *(o.symbol);
+
+    return os;
 }
